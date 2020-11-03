6dbb85d685021e6dd869a265a3dfc1cedc0e0d3e
lars <60745347+lars-ruijs@users.noreply.github.com>
2020-11-03 11:19:06 +0100

    Extract city names

--- rdw-data/scripts/script.js
+++ rdw-data/scripts/script.js
@@ -2,12 +2,16 @@
 // Endpoint of Park and Ride locations in the Netherlands
 const endpoint = "https://opendata.rdw.nl/resource/6wzd-evwu.json";
 
-const parkeerData = getData(endpoint)
+getData(endpoint)
 	.then(result => {
 	return result.json();
 })
 .then(RDWData => {
 	console.log(RDWData);
+	const areaDesc = filterData(RDWData, "areadesc");
+	console.log(areaDesc);
+	const steden = getCityName(areaDesc);
+	console.log(steden);
 });
 
 // Fetch the data from a given url (endpoint)
@@ -17,5 +21,21 @@ function getData(url){
 
 // Returns all values for a certain key in an array of data
 function filterData(dataArray, key){
-  return dataArray.map(item => item[key]);
+  return dataArray.map(item => item[key]).filter(isEmpty);
+}
+
+// Remove empty values from the array
+function isEmpty(value) {
+    if (value) {
+        return true; // Return value if it's not empty
+    }
+    else {
+        return false; // Remove values that are empty 
+    }
+}
+
+// Extract the city names of P+R locations that are inside parenthesis and return those as a new array
+// RegEx code adapted from https://stackoverflow.com/questions/17779744/regular-expression-to-get-a-string-between-parentheses-in-javascript
+function getCityName(dataItem) {
+	return dataItem.map(item => /\(([^)]+)\)/.exec(item)[1]);
 }
